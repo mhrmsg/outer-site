96c91ee60b36cd991199bec0b765578cb1b84dde
mhrmsg <908155169@qq.com>
2025-07-12 16:42:11 +0800

    feat:模型加载以及处理

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -10,8 +10,12 @@ import {
 
 export default function Experience() {
     const computer = useGLTF(
-        "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+        // "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+        // "/old_computer/scene.gltf"
+        "/desktop_computer/scene.gltf"
     );
+
+    console.log(computer);
 
     return (
         <>
@@ -27,42 +31,42 @@ export default function Experience() {
                 damping={0.1}
                 snap
             >
-                <Float rotationIntensity={0.4}>
-                    <rectAreaLight
-                        width={2.5}
-                        height={1.65}
-                        intensity={65}
-                        color={"#ff6900"}
-                        rotation={[-0.1, Math.PI, 0]}
-                        position={[0, 0.55, -1.15]}
-                    />
+                {/* <Float rotationIntensity={0.4}> */}
+                {/* <rectAreaLight
+                    width={2.5}
+                    height={1.65}
+                    intensity={65}
+                    color={"#ff6900"}
+                    rotation={[-0.1, Math.PI, 0]}
+                    position={[0, 0.55, -1.15]}
+                /> */}
 
-                    <primitive
-                        object={computer.scene}
-                        position-y={-1.2}
-                        // rotation-x={ 0.13 }
+                <primitive
+                    object={computer.scene}
+                    position={[0.2, -1.1, -0.1]}
+                    rotation={[-0.15, -0.68, -0.04]}
+                >
+                    <Html
+                        transform
+                        wrapperClass="htmlScreen"
+                        distanceFactor={1.17}
+                        position={[-0, 1.45, -0.24]}
+                        rotation={[-0, -0, -0.0]}
                     >
-                        <Html
-                            transform
-                            wrapperClass="htmlScreen"
-                            distanceFactor={1.17}
-                            position={[0, 1.56, -1.4]}
-                            rotation-x={-0.256}
-                        >
-                            <iframe src="http://localhost:5174/" />
-                        </Html>
-                    </primitive>
+                        <iframe src="http://localhost:5174/" />
+                    </Html>
+                </primitive>
 
-                    <Text
-                        font="./bangers-v20-latin-regular.woff"
-                        fontSize={1}
-                        position={[1, 0.75, 0.75]}
-                        rotation-y={-1.25}
-                        maxWidth={2}
-                    >
-                        MHR
-                    </Text>
-                </Float>
+                <Text
+                    font="./bangers-v20-latin-regular.woff"
+                    fontSize={1}
+                    position={[3, 0.15, 1.85]}
+                    rotation-y={-1.25}
+                    maxWidth={2}
+                >
+                    MHR
+                </Text>
+                {/* </Float> */}
             </PresentationControls>
 
             <ContactShadows
